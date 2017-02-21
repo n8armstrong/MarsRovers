ee63ae269c339bee5156139085389b8d92c301dd
Nate Armstrong <narmstrong@instructure.com>
2017-02-21 16:15:12 -0700

    [MarsRovers] Add photosReducer test

---
+++ src/reducers/__tests__/photos.js
@@ -0,0 +1,17 @@
+import { PHOTO_REQUEST_STARTED } from '../../actions/photos'
+import photosReducer from '../../reducers/photos'
+
+describe('photosReducer', () => {
+  test('request started', () => {
+    const stateBefore = {}
+    const stateAfter = {
+      photosPending: true,
+      photosSuccessful: false,
+      photosError: null
+    }
+
+    expect(
+      photosReducer(stateBefore, {type: PHOTO_REQUEST_STARTED})
+    ).toEqual(stateAfter)
+  })
+})
